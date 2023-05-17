dfad9d18de86f03c724caf64156e6dad49a0f9ef
Guilherme Ricioli <guilherme.riciolic@gmail.com>
2023-05-17 15:40:23 -0300

    Module: first snapshot
    Module is a template class providing the basics for thread-based modules
    using Mbed OS.

---
+++ Module/Module.hpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2021-2023 CNPEM (cnpem.br)
+ * Author: Guilherme Ricioli <guilherme.ricioli@lnls.br>
+ */
+
+#ifndef COMMON_MODULE_HPP_
+#define COMMON_MODULE_HPP_
+
+#include "mbed.h"
+
+#include "Message.hpp"
+
+/// TODO: Should this be the same for all modules?
+#define BUFF_TIMEOUT_MS 1
+
+namespace common {
+    template<uint32_t BUFF_SIZE>
+    class Module {
+        public:
+            Module(module_id_t id, osPriority priority, uint32_t stack_size,
+                unsigned char *stack_mem, const char *name) {
+                this->id        = id;
+                this->thread    = new Thread(priority, stack_size, stack_mem,
+                    name);
+                this->occup     = new Semaphore(0);
+                this->unoccup   = new Semaphore(BUFF_SIZE);
+            }
+
+            virtual ~Module() {
+                delete this->thread;
+                delete this->occup;
+                delete this->unoccup;
+            }
+
+            bool start() {
+                bool status = false;
+                mbed::Callback<void()> cb = callback(this, &Module::task);
+                status = (this->thread->start(cb) == osOK)? true : false;
+
+                return status;
+            }
+
+            bool put_msg(const Message &msg) {
+                bool status = false;
+                std::chrono::milliseconds timeout(BUFF_TIMEOUT_MS);
+                if(this->unoccup->try_acquire_for(timeout) == true) {
+                    this->buff.push(msg);
+                    status = (this->occup->release() == osOK)? true : false;
+                } else {
+                    status = false;
+                }
+
+                return status;
+            }
+
+            bool get_msg(Message &msg) {
+                bool status = false;
+                this->occup->acquire();
+                if(this->buff.pop(msg) == true) {
+                    status = (this->unoccup->release() == osOK)? true : false;
+                } else {
+                    status = false;
+                }
+
+                return status;
+            }
+
+        private:
+            module_id_t                                     id;
+            rtos::Thread                                    *thread;
+            rtos::Semaphore                                 *occup;
+            rtos::Semaphore                                 *unoccup;
+            mbed::CircularBuffer<Message, BUFF_SIZE>        buff;
+
+            virtual void task() = 0;
+    };
+} // namespace common
+
+#endif /* COMMON_MODULE_HPP_ */
