abe321cc4bbbb30771586fc575cdb14fd9847f57
Guilherme Ricioli <guilherme.riciolic@gmail.com>
2023-05-23 13:03:03 -0300

    Module.hpp: add descriptions

--- Module/Module.hpp
+++ Module/Module.hpp
@@ -10,9 +10,15 @@
 
 #define BUFF_TIMEOUT_MS std::chrono::duration<uint32_t, std::milli>(1)
 
+/* Module is a template class providing the basics for thread-based modules.
+ */
 template<typename T, uint32_t BUFF_SIZE>
 class Module {
   public:
+    /* NOTE: One can dinamically allocate the stack memory by passing
+     *       'stack_mem' as nullptr. This is not recomended as it may lead to
+     *       memory fragmentation. Static allocation is preferred.
+     */
     Module(osPriority priority, uint32_t stack_size, unsigned char *stack_mem,
         const char *name) {
       _p_thread = new rtos::Thread(priority, stack_size, stack_mem, name);
@@ -24,12 +30,16 @@ class Module {
       delete _p_buff_busy;
     }
 
+    /* Spawns a thread assigning internal method _task() for it to run. */
     bool start() {
       mbed::Callback<void()> cb = callback(this, &Module::_task);
 
       return _p_thread->start(cb) == osOK? true : false;
     }
 
+    /* Stores a message on internal buffer.
+     * This is the inter-thread communication mechanism.
+     */
     bool store_msg(const T &msg) {
       bool status = false;
 
@@ -45,6 +55,8 @@ class Module {
     }
 
   protected:
+    /* Reads a message from internal buffer.
+     */
     bool _read_msg(T &msg) {
       bool status = false;
 
@@ -63,6 +75,9 @@ class Module {
     rtos::Semaphore *_p_buff_busy;
     mbed::CircularBuffer<T, BUFF_SIZE> _buff;
 
+    /* A method to be executed by the internal thread when start() is called.
+     * NOTE: Inherited classes must implement this.
+     */
     virtual void _task() = 0;
 };
 
